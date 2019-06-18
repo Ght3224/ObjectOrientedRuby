b4a03e7ca9e558104498e7d942a49f72a8908ee6
YOUR NAME <adien330@yahoo.com>
2019-06-17 22:12:18 -0400

    first OO file

---
+++ item.rb
@@ -0,0 +1,15 @@
+
+item1 = {name: "jar",
+ price: 300, 
+color: "blue"}
+
+item2 = {:name => "pin",
+ :price => 400, 
+:color => "green"}
+
+item3 = {name: "disc",
+ price: 3000, 
+color: "yellow"}
+
+
+p "#{item1[:name]}, #{item2[:name]}, #{item3[:name]}"
