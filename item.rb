be72cafb9b7641d90ad744216bde09e93a67881b
YOUR NAME <adien330@yahoo.com>
2019-06-18 19:45:41 -0400

    Create classes, create instance, 3 puts statements...

--- item.rb
+++ item.rb
@@ -1,15 +1,57 @@
 
-item1 = {name: "jar",
- price: 300, 
-color: "blue"}
+# item1 = {name: "jar",
+#  price: 300, 
+# color: "blue"}
 
-item2 = {:name => "pin",
- :price => 400, 
-:color => "green"}
+# item2 = {:name => "pin",
+#  :price => 400, 
+# :color => "green"}
 
-item3 = {name: "disc",
- price: 3000, 
-color: "yellow"}
+# item3 = {name: "disc",
+#  price: 3000, 
+# color: "yellow"}
 
 
-p "#{item1[:name]}, #{item2[:name]}, #{item3[:name]}"
+# p "#{item1[:name]}, #{item2[:name]}, #{item3[:name]}"
+
+
+
+class Store 
+
+
+  def initialize(name, price, color)
+    @name = name 
+    @price = price 
+    @color = color 
+  end 
+
+
+  def name 
+    @name 
+  end 
+
+  def price 
+    @price 
+  end 
+
+  def price=(price)
+    @price = price 
+  end 
+
+  def color 
+    @color
+  end 
+
+  def color=(color) 
+    @color = color
+  end 
+
+end 
+
+item1=Store.new("disc", 300, "blue")
+# item2=Store.new("pin", 400, "green")
+# item3=Store.new("disc", 3000, "yellow")
+
+p item1.name
+p item1.price
+p item1.color
