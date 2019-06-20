c8739478b4a8302579412bc2faff613cac56ccec
YOUR NAME <adien330@yahoo.com>
2019-06-19 20:29:56 -0400

    using modules instead of classes

---
+++ module_example.rb
@@ -0,0 +1,46 @@
+module  Vehicle 
+attr_accessor :speed, :direction 
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+end 
+
+class Car
+  include Vehicle 
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+
+include Vehicle 
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+
+Bicycle1=Bike.new 
+Car1=Car.new
+
+Bicycle1.accelerate 
+p Bicycle1.speed
+Bicycle1.ring_bell
+Car1.honk_horn  
